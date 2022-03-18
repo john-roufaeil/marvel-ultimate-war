8e66414bf8ce74f39a8f1ad9b1fb3f344bbc47e3
john <john.flouis1@gmail.com>
2022-03-18 19:16:48 +0200

    add Direction enum, Condition enum, Cover class

---
+++ src/guc/game/model/world/Cover.java
@@ -0,0 +1,29 @@
+package guc.game.model.world;
+
+import java.awt.Point;
+
+public class Cover {
+	// instance variables
+	private int currentHP;
+	private Point location; 
+	
+	// constructors
+	public Cover(int x, int y) {
+		currentHP = 100 + (int) (Math.random() * (900));
+		location = new Point(x,y);
+	}
+	
+	// getters and setters
+	public int getCurrentHP() {
+		return currentHP;
+	}
+
+	public void setCurrentHP(int currentHP) {
+		this.currentHP = currentHP;
+	}
+
+
+	public Point getLocation() {
+		return location;
+	}
+}
