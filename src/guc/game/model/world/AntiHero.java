bc5c17efe04f123dbf83a49693c96f8e39f53a3f
john <john.flouis1@gmail.com>
2022-03-18 23:35:24 +0200

    add Champion, AntiHero, Hero, Villain classes

---
+++ src/guc/game/model/world/AntiHero.java
@@ -0,0 +1,8 @@
+package guc.game.model.world;
+
+public class AntiHero extends Champion {
+
+	public AntiHero(String name, int maxHP, int mana, int maxActions, int speed, int attackRange, int attackDamage) {
+		super(name, maxHP, mana, maxActions, speed, attackRange, attackDamage);
+	}
+}
