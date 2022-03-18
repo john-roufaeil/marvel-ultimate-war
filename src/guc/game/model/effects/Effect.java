5cfbbb437da2e88fb0e93ce73646d0bc1bb341b7
john <john.flouis1@gmail.com>
2022-03-18 17:22:39 +0200

    Revert "update hierarchy"
    This reverts commit 33597bc3dd80ca0f44e948c92b617c150afa85cc.

--- src/guc/game/model/effects/Effect.java
+++ src/guc/game/model/effects/Effect.java
@@ -1,21 +1,10 @@
 package guc.game.model.effects;
 
 public class Effect {
-	// instance variables
 	private String name;
 	private int duration;
 	private EffectType type;
 	
-	// constructors
-	public Effect(String name, int duration, EffectType type) {
-		this.name = name;
-		this.duration = duration;
-		this.type = type;
-	}
-	
-	public Effect(int duration) {
-		this.duration = duration;
-	}
 	
 	// getters and setters
 	public String getName() {
@@ -33,10 +22,4 @@ public class Effect {
 	public EffectType getType() {
 		return this.type;
 	}
-
-	public void setType(EffectType type) {
-		this.type = type;
-	}
-	
-	// methods
 }
