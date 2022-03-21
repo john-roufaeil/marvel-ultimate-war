2571afe8b7da82e15613c4948c89a2e454f8143a
John Fayez Louis <97978852+John-louis1@users.noreply.github.com>
2022-03-21 13:05:27 +0200

    Update Disarm.java

--- src/guc/game/model/effects/Disarm.java
+++ src/guc/game/model/effects/Disarm.java
@@ -2,8 +2,8 @@ package guc.game.model.effects;
 
 public class Disarm extends Effect {
 
-	public Disarm(String name, int duration) {
-		super(name, duration, EffectType.DEBUFF);
+	public Disarm(int duration) {
+		super("Disarm", duration, EffectType.DEBUFF);
 	}
 	
 	
