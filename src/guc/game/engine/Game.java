5208fbf0103673491e47d5c8fefad65721f51724
john <john.flouis1@gmail.com>
2022-03-23 13:14:00 +0200

    loadAbilities method in Game.java, add csv files

--- src/guc/game/engine/Game.java
+++ src/guc/game/engine/Game.java
@@ -1,16 +1,19 @@
 package guc.game.engine;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 import guc.game.model.world.*;
 import guc.game.model.abilities.*;
+import guc.game.model.effects.Effect;
+import guc.game.model.effects.EffectType;
 
 public class Game {
 	private Player firstPlayer;
 	private Player secondPlayer;
 	private boolean firstLeaderAbilityUsed;
 	private boolean secondLeaderAbilityUsed;
-	private Object[][] board; // to be initalized in the constructor [5][5]
+	private Object[][] board;
 	private static ArrayList<Champion> availableChampions;
 	private static ArrayList<Ability> availableAbilities;
 	private PriorityQueue turnOrder;
@@ -18,7 +21,7 @@ public class Game {
 	private static int BOARDWIDTH;
 
 	// constructors
-	public Game(Player first,Player second) {
+	public Game(Player first,Player second) throws Exception {
 	   firstPlayer = first;
 	   secondPlayer = second;
 	   board = new Object[5][5];
@@ -67,35 +70,57 @@ public class Game {
 		return turnOrder;
 	}
 	
-	private void placeChampions() {
+	private void placeChampions() throws Exception {
 		
 	}
 	
-	private void placeCovers() {
+	private void placeCovers() throws Exception {
 		for (int i = 0; i < 5; i++) {
 			int x;
 			int y;
 			do {
-				x = (int) (Math.random() * BOARDWIDTH); // from 0 inc to 4 inc
-				y = (int) (Math.random() * (BOARDHEIGHT - 2)) + 1; // from 1 inc to 3 inc
+				x = (int) (Math.random() * 5); // from 0 inc to 4 inc
+				y = (int) (Math.random() * 3) + 1; // from 1 inc to 3 inc
 			} while (this.board[x][y] != null);
 			Cover c = new Cover(x,y);
 			this.board[x][y] = c;
 		}
 	}
 	
-//	private void placeCovers() {
-//		for (int i = 0; i < 5; i++) {
-//			int x;
-//			int y;
-//			do {
-//				x = (int) (Math.random() * 5); // from 0 inc to 4 inc
-//				y = (int) (Math.random() * 3) + 1; // from 1 inc to 3 inc
-//			} while (this.board[x][y] != null);
-//			Cover c = new Cover(x,y);
-//			this.board[x][y] = c;
-//		}
-//	}
+	private static void loadAbilities(String filePath) throws Exception {
+		BufferedReader abilitiesBR = new BufferedReader(new FileReader(filePath));
+		String s;
+		String arr[];
+		DamagingAbility d;
+		HealingAbility h;
+		CrowdControlAbility c;
+		do {
+			s = abilitiesBR.readLine();
+			arr =s.split(",");
+			if (arr[0] == "DMG") {
+				d = new DamagingAbility(arr[1], Integer.parseInt(arr[2]), Integer.parseInt(arr[3]), Integer.parseInt(arr[4]), AreaOfEffect.valueOf(arr[5]), Integer.parseInt(arr[6]), Integer.parseInt(arr[7]));
+				availableAbilities.add(d);
+			}
+			
+			else if (arr[0] == "HEL") {
+				h = new HealingAbility(arr[1], Integer.parseInt(arr[2]), Integer.parseInt(arr[3]), Integer.parseInt(arr[4]), AreaOfEffect.valueOf(arr[5]), Integer.parseInt(arr[6]), Integer.parseInt(arr[7]));
+				availableAbilities.add(h);
+			}
+			
+			else {
+				
+				Effect e = new Effect(arr[7], Integer.parseInt(arr[8]),EffectType.valueOf(arr[7]));
+				c = new CrowdControlAbility(arr[1], Integer.parseInt(arr[2]), Integer.parseInt(arr[3]), Integer.parseInt(arr[4]), AreaOfEffect.valueOf(arr[5]), Integer.parseInt(arr[6]), e);
+				availableAbilities.add(c);
+			}
+	
+		} while (s != null);
+				
+	}
+	
+	private static void loadChampions(String filePath) throws Exception {
+		BufferedReader championsBR = new BufferedReader(new FileReader(filePath));
+	}
 	
 	
 }
