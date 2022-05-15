4921b390d30f0158f964cc2a5d2b467572344163
john <john.flouis1@gmail.com>
2022-05-16 01:28:24 +0200

    edit priorityQueue order

--- src/engine/PriorityQueue.java
+++ src/engine/PriorityQueue.java
@@ -15,7 +15,7 @@ public class PriorityQueue {
 	public void insert(Comparable item) {
 
 		int i;
-		for (i = nItems - 1; i >= 0 && item.compareTo(elements[i]) > 0; i--)
+		for (i = nItems - 1; i >= 0 && item.compareTo(elements[i]) < 0; i--)
 			elements[i + 1] = elements[i];
 
 		elements[i + 1] = item;
@@ -42,4 +42,20 @@ public class PriorityQueue {
 	public int size() {
 		return nItems;
 	}
+	
+//	public static void main(String [] args) {
+//		PriorityQueue q = new PriorityQueue(6);
+//		q.insert(1);
+//		System.out.println(q.peekMin());
+//		q.insert(2);
+//		System.out.println(q.peekMin());
+//		q.insert(3);
+//		System.out.println(q.peekMin());
+//		q.insert(50);
+//		System.out.println(q.peekMin());
+//		q.insert(60);
+//		System.out.println(q.peekMin());
+//		q.insert(55);
+//		System.out.println(q.peekMin());
+//	}
 }
