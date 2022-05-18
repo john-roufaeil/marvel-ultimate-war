8e4b3851b76ec69b962511b282cfdf0bece2d2ce
john <john.flouis1@gmail.com>
2022-05-18 09:48:11 +0200

    add exceptions

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+import engine.*;
+import model.*;
+import exceptions.*;
+
+
+public class Main {
+	public static void main(String [] args) throws IOException {
+		Player first = new Player("Youssef");
+		Player second = new Player("Husseiny");
+		Game.loadAbilities("Abilities.csv");
+		Game.loadChampions("Champions.csv");
+		Game g = new Game(first, second);
+		
+		//	System.out.println(g.getAvailableAbilities());
+		//System.out.println(g.getCurrentChampion());
+		
+	}
+}
